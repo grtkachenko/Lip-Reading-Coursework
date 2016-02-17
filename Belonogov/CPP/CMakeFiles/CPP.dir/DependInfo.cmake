eaa055fab5268bdf5ab23a0c3a99c4a10b01e5be
Belonogov Ivan <Belonogov95@gmail.com>
2016-02-17 14:40:50 +0300

    reorganize project

---
+++ Belonogov/CPP/CMakeFiles/CPP.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/main.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/CMakeFiles/CPP.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/vanya/soft/lib/dlib-18.18/dlib/.."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vanya/soft/lib/dlib-18.18/dlib/base64/base64_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/base64/base64_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/bigint/bigint_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/bigint/bigint_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/bigint/bigint_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/bigint/bigint_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/bit_stream/bit_stream_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/bit_stream/bit_stream_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/bsp/bsp.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/bsp/bsp.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/data_io/image_dataset_metadata.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/data_io/image_dataset_metadata.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/dir_nav/dir_nav_extensions.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_extensions.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/dir_nav/dir_nav_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/dir_nav/dir_nav_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/entropy_decoder/entropy_decoder_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/entropy_decoder/entropy_decoder_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/entropy_decoder/entropy_decoder_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/entropy_decoder/entropy_decoder_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/entropy_encoder/entropy_encoder_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/entropy_encoder/entropy_encoder_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/entropy_encoder/entropy_encoder_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/entropy_encoder/entropy_encoder_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_core/gui_core_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_core/gui_core_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_core/gui_core_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_core/gui_core_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_widgets/base_widgets.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_widgets/base_widgets.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_widgets/canvas_drawing.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_widgets/canvas_drawing.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_widgets/drawable.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_widgets/drawable.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_widgets/fonts.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_widgets/fonts.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_widgets/style.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_widgets/style.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/gui_widgets/widgets.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/gui_widgets/widgets.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/image_loader/jpeg_loader.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/image_loader/jpeg_loader.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/image_loader/png_loader.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/image_loader/png_loader.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/image_saver/save_jpeg.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/image_saver/save_jpeg.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/image_saver/save_png.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/image_saver/save_png.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/linker/linker_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/linker/linker_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/logger/extra_logger_headers.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/logger/extra_logger_headers.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/logger/logger_config_file.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/logger/logger_config_file.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/logger/logger_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/logger/logger_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/md5/md5_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/md5/md5_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/misc_api/misc_api_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/misc_api/misc_api_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/misc_api/misc_api_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/misc_api/misc_api_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/server/server_http.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/server/server_http.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/server/server_iostream.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/server/server_iostream.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/server/server_kernel.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/server/server_kernel.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/sockets/sockets_extensions.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_extensions.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/sockets/sockets_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/sockets/sockets_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/sockstreambuf/sockstreambuf.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/sockstreambuf/sockstreambuf.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/sockstreambuf/sockstreambuf_unbuffered.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/sockstreambuf/sockstreambuf_unbuffered.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/stack_trace.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/stack_trace.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/threads/multithreaded_object_extension.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/threads/multithreaded_object_extension.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/threads/thread_pool_extension.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/threads/thread_pool_extension.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/threads/threaded_object_extension.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/threads/threaded_object_extension.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/threads/threads_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/threads/threads_kernel_2.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_2.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/threads/threads_kernel_shared.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_shared.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/timer/timer.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/timer/timer.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/tokenizer/tokenizer_kernel_1.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/tokenizer/tokenizer_kernel_1.o"
+  "/home/vanya/soft/lib/dlib-18.18/dlib/unicode/unicode.cpp" "/home/vanya/Documents/LR/4/Lip-Reading-Coursework/Belonogov/CPP/dlib_build/CMakeFiles/dlib.dir/unicode/unicode.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/vanya/soft/lib/dlib-18.18/dlib/.."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
